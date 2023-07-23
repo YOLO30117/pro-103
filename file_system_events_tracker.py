e7aa522a7c997eb1a431d1ba4264cb7e947c0cef
YOLO30117 <71224112+YOLO30117@users.noreply.github.com>
2023-07-23 11:44:58 +0530

    Add files via upload

---
+++ file_system_events_tracker.py
@@ -0,0 +1,40 @@
+import sys
+import os
+import random
+import time
+import shutil
+import logging
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from_dir = "C:/Users/Lolo/Downloads"
+
+class FileEventHandler(FileSystemEventHandler):
+     def on_created(self, event):
+        print(f"Hey, {event.src_path} has been created!")
+
+     def on_deleted(self, event):
+        print(f"Oops! Someone deleted {event.src_path}!")
+
+     def on_modified(self, event):
+        print(f"Someone modified {event.src_path}!")
+
+     def on_moved(self, event):
+        print(f"Oops! Someone moved {event.src_path}!")
+
+event_handler=FileEventHandler()
+
+observer=Observer()
+
+observer.schedule(event_handler,from_dir,recursive=True)
+
+observer.start()
+
+try:
+   while True:
+      time.sleep(2)
+      print("running...")
+
+except KeyboardInterrupt:
+   print("stopped")
+   observer.stop()
